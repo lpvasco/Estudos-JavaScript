4853f130125e599c037fb3744b1b2472ea16587a
lpvasco <lucasp_vasco@hotmail.com>
2022-08-05 16:54:08 -0300

    trocarValor e maiorValor - desafios

---
+++ maiorValor.js
@@ -0,0 +1,9 @@
+const valor1 = 5;
+const valor2 = 10;
+
+if (valor1 > valor2) {
+    console.log(valor1, ' é maior que', valor2);
+}
+else {
+    console.log(valor2, ' é maior que', valor1);
+}
---
+++ trocarValor.js
@@ -0,0 +1,8 @@
+// let a =  'azul';
+// let b = 'laranja';
+// let c = a;
+// a = b;
+// b = c;
+
+// console.log(a);
+// console.log(b);
