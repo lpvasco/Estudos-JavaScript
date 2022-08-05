152154950a77d6a45ec7c7a25806d3cea8b8eec6
lpvasco <lucasp_vasco@hotmail.com>
2022-08-05 18:02:42 -0300

    fizzBuzz em JavaScript

--- maiorValor.js
+++ maiorValor.js
@@ -1,19 +1,19 @@
-const valor1 = 5;
-const valor2 = 10;
+// const valor1 = 5;
+// const valor2 = 10;
 
-if (valor1 > valor2) {
-    console.log(valor1, ' é maior que', valor2);
-}
-else {
-    console.log(valor2, ' é maior que', valor1);
-}
- //OU
+// if (valor1 > valor2) {
+//     console.log(valor1, ' é maior que', valor2);
+// }
+// else {
+//     console.log(valor2, ' é maior que', valor1);
+// }
+//  //OU
 
- let maiorValor = max(5,9);
- console.log(maiorValor);
+//  let maiorValor = max(5,9);
+//  console.log(maiorValor);
 
- function max(numero1,numero2) {
-    if(numero1 > numero2)
-        return numero1;
-    else return numero2;
- }
+//  function max(numero1,numero2) {
+//     if(numero1 > numero2)
+//         return numero1;
+//     else return numero2;
+//  }
