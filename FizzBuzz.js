df99388ac4fd3b669fb8747ccfb76792a92794e4
lpvasco <lucasp_vasco@hotmail.com>
2022-08-08 14:19:31 -0300

    function simulador de radar

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,17 +1,17 @@
-// return Fizz se o valor for divisivel por 3
-//return Buzz se o valor for divisivel por 5
-const num = 0;
-const resultado = fizzBuzz(15);
-    console.log(resultado);
+// // return Fizz se o valor for divisivel por 3
+// //return Buzz se o valor for divisivel por 5
+// const num = 0;
+// const resultado = fizzBuzz(15);
+//     console.log(resultado);
 
-function fizzBuzz(input){
-    if(typeof input !== 'number')
-        return 'Não é um número'
-    else if(input % 3 === 0 && input % 5 === 0)
-        return 'FizzBuzz';
-    else if(input % 3 === 0)
-        return 'Fizz';
-    else if(input % 5 === 0)
-        return 'Buzz';
-    return input;
-}
+// function fizzBuzz(input){
+//     if(typeof input !== 'number')
+//         return 'Não é um número'
+//     else if(input % 3 === 0 && input % 5 === 0)
+//         return 'FizzBuzz';
+//     else if(input % 3 === 0)
+//         return 'Fizz';
+//     else if(input % 5 === 0)
+//         return 'Buzz';
+//     return input;
+// }
---
+++ velocidadeMaxima.js
@@ -0,0 +1,13 @@
+// verificarVelocidade(80);
+
+// function verificarVelocidade(velocidade) {
+//     if (velocidade <= 70)
+//         console.log('Ok');
+//     else {
+//         const pontos = Math.floor(((velocidade - 70) / 5))
+//         if (pontos >= 12)
+//             console.log('Carteira Suspensa')
+//         else
+//             console.log('Você recebeu '+ pontos + ' pontos na carteira');
+//     }
+// }
